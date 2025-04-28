d594bd39b64ada0f039833b815dfac627f516b99
7gahunguflorian <120568555+7gahunguflorian@users.noreply.github.com>
2025-04-28 18:39:22 +0200

    Initial commit

---
+++ triage_dans_un_fichier_texte.py
@@ -0,0 +1,11 @@
+with open("C:/Users/gahun/Downloads/prenom.txt", "r") as f:
+    lines = f.read().splitlines()
+
+prenoms = []
+for line in lines:
+    prenoms.extend(line.split())
+
+prenoms_final = [prenom.strip(",. ") for prenom in prenoms]
+
+with open("C:/Users/gahun/Downloads/prenoms_final.txt", "w") as f:
+    f.write("\n".join(sorted(prenoms_final)))
